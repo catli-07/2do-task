a0ef2563a65e845048314a508ab4afbc8ca86f7d
catli-07 <catli.01100101@gmail.com>
2023-02-02 22:25:03 +0800

    2nd commit

---
+++ src/WebUI/ClientApp/src/app/todo/todo.component.ts
@@ -0,0 +1,337 @@
+import { Component, TemplateRef, OnInit,Input, OnChanges, SimpleChanges} from '@angular/core';
+import { UntypedFormBuilder } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import {
+  TodoListsClient, TodoItemsClient,
+  TodoListDto, TodoItemDto, PriorityLevelDto,
+  CreateTodoListCommand, UpdateTodoListCommand,
+  CreateTodoItemCommand, UpdateTodoItemDetailCommand
+} from '../web-api-client';
+import { style } from '@angular/animations';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-component',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+
+  
+})
+export class TodoComponent implements OnInit {
+  debug = false;
+  deleting = false;
+  deleteCountDown = 0;
+  deleteCountDownInterval: any;
+  lists: TodoListDto[];
+  priorityLevels: PriorityLevelDto[];
+  selectedList: TodoListDto;
+  selectedItem: TodoItemDto;
+  newListEditor: any = {};
+  listOptionsEditor: any = {};
+  newListModalRef: BsModalRef;
+  listOptionsModalRef: BsModalRef;
+  deleteListModalRef: BsModalRef;
+  itemDetailsModalRef: BsModalRef;
+  color: any = {};
+  @Input() colorRecieved: string;
+  term: string;
+  itemDetailsFormGroup = this.fb.group({
+    id: [null],
+    listId: [null],
+    priority: [''],
+    note: ['']
+  });
+
+
+  constructor(
+    private listsClient: TodoListsClient,
+    private itemsClient: TodoItemsClient,
+    private modalService: BsModalService,
+    private fb: UntypedFormBuilder
+  ) { }
+
+
+  ngOnInit(): void {
+    this.listsClient.get().subscribe(
+      result => {
+        this.lists = result.lists;
+        this.priorityLevels = result.priorityLevels;
+        if (this.lists.length) {
+          this.selectedList = this.lists[0];
+        }
+      },
+      error => console.error(error)
+    );
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //     if (changes?.colorRecieved) {
+  //       console.log(this.colorRecieved); // this is added for demo purpouses
+  //     }
+  //   }
+
+    ngOnChanges(changes: SimpleChanges) {
+      if (changes?.colorRecieved) {
+        console.log(this.colorRecieved); 
+        // this is added for demo purpouses
+      }
+    }
+
+    
+
+    // select_color(color){
+    //   this.color = color;
+    // }
+
+    
+    items:any[];
+    
+    changeColors(item:TodoItemDto){
+      this.selectedItem = item;
+      const itemIndex = this.selectedList.items.indexOf(this.selectedItem)
+    // I'm just selecting the first row for this example but you have to define your own logic 
+       setTimeout(() => document.getElementsByClassName('todo4').item, 250);
+    }
+
+   
+
+   
+
+    
+
+   deleteItemRow(item:TodoItemDto){
+    const itemIndex = this.selectedList.items.indexOf(this.selectedItem);
+    this.selectedList.items.splice(itemIndex, 1);
+    this.itemsClient.delete(item.id).subscribe(
+      () =>
+      (this.selectedList.items = this.selectedList.items.filter(
+        t => t.id !== item.id
+      )),
+      error => console.error(error)
+    );
+   }
+
+   deleteListRow(list:TodoListDto){
+    const itemIndex = this.lists.indexOf(this.selectedList)
+    this.lists.splice(itemIndex, 1);
+    this.listsClient.delete(list.id).subscribe(
+      () =>
+      (this.lists = this.lists.filter(
+        t => t.id !== list.id
+      )),
+      error => console.error(error)
+    );
+
+   }
+
+
+  
+
+  // Lists
+  remainingItems(list: TodoListDto): number {
+    return list.items.filter(t => !t.done).length;
+  }
+
+  showNewListModal(template: TemplateRef<any>): void {
+    this.newListModalRef = this.modalService.show(template);
+    setTimeout(() => document.getElementById('title').focus(), 250);
+  }
+
+  newListCancelled(): void {
+    this.newListModalRef.hide();
+    this.newListEditor = {};
+  }
+
+  addList(): void {
+    const list = {
+      id: 0,
+      title: this.newListEditor.title,
+      items: []
+    } as TodoListDto;
+
+    this.listsClient.create(list as CreateTodoListCommand).subscribe(
+      result => {
+        list.id = result;
+        this.lists.push(list);
+        this.selectedList = list;
+        this.newListModalRef.hide();
+        this.newListEditor = {};
+      },
+      error => {
+        const errors = JSON.parse(error.response);
+
+        if (errors && errors.Title) {
+          this.newListEditor.error = errors.Title[0];
+        }
+
+        setTimeout(() => document.getElementById('title').focus(), 250);
+      }
+    );
+  }
+
+  showListOptionsModal(template: TemplateRef<any>) {
+    this.listOptionsEditor = {
+      id: this.selectedList.id,
+      title: this.selectedList.title
+    };
+
+    this.listOptionsModalRef = this.modalService.show(template);
+  }
+
+  updateListOptions() {
+    const list = this.listOptionsEditor as UpdateTodoListCommand;
+    this.listsClient.update(this.selectedList.id, list).subscribe(
+      () => {
+        (this.selectedList.title = this.listOptionsEditor.title),
+          this.listOptionsModalRef.hide();
+        this.listOptionsEditor = {};
+      },
+      error => console.error(error)
+    );
+  }
+
+  confirmDeleteList(template: TemplateRef<any>) {
+    this.listOptionsModalRef.hide();
+    this.deleteListModalRef = this.modalService.show(template);
+  }
+
+  deleteListConfirmed(): void {
+    this.listsClient.delete(this.selectedList.id).subscribe(
+      () => {
+        this.deleteListModalRef.hide();
+        this.lists = this.lists.filter(t => t.id !== this.selectedList.id);
+        this.selectedList = this.lists.length ? this.lists[0] : null;
+      },
+      error => console.error(error)
+    );
+  }
+
+  // Items
+  showItemDetailsModal(template: TemplateRef<any>, item: TodoItemDto): void {
+    this.selectedItem = item;
+    this.itemDetailsFormGroup.patchValue(this.selectedItem);
+
+    this.itemDetailsModalRef = this.modalService.show(template);
+    this.itemDetailsModalRef.onHidden.subscribe(() => {
+        this.stopDeleteCountDown();
+    });
+  }
+
+  updateItemDetails(): void {
+    const item = new UpdateTodoItemDetailCommand(this.itemDetailsFormGroup.value);
+    this.itemsClient.updateItemDetails(this.selectedItem.id, item).subscribe(
+      () => {
+        if (this.selectedItem.listId !== item.listId) {
+          this.selectedList.items = this.selectedList.items.filter(
+            i => i.id !== this.selectedItem.id
+          );
+          const listIndex = this.lists.findIndex(
+            l => l.id === item.listId
+          );
+          this.selectedItem.listId = item.listId;
+          this.lists[listIndex].items.push(this.selectedItem);
+        }
+
+        this.selectedItem.priority = item.priority;
+        this.selectedItem.note = item.note;
+        this.itemDetailsModalRef.hide();
+        this.itemDetailsFormGroup.reset();
+      },
+      error => console.error(error)
+    );
+  }
+
+  addItem() {
+    const item = {
+      id: 0,
+      listId: this.selectedList.id,
+      priority: this.priorityLevels[0].value,
+      title: '',
+      done: false
+    } as TodoItemDto;
+
+    this.selectedList.items.push(item);
+    const index = this.selectedList.items.length - 1;
+    this.editItem(item, 'itemTitle' + index);
+  }
+
+  editItem(item: TodoItemDto, inputId: string): void {
+    this.selectedItem = item;
+    setTimeout(() => document.getElementById(inputId).focus(), 100);
+  }
+
+  updateItem(item: TodoItemDto, pressedEnter: boolean = false): void {
+    const isNewItem = item.id === 0;
+
+    if (!item.title.trim()) {
+      this.deleteItem(item);
+      return;
+    }
+
+    if (item.id === 0) {
+      this.itemsClient
+        .create({
+          ...item, listId: this.selectedList.id
+        } as CreateTodoItemCommand)
+        .subscribe(
+          result => {
+            item.id = result;
+          },
+          error => console.error(error)
+        );
+    } else {
+      this.itemsClient.update(item.id, item).subscribe(
+        () => console.log('Update succeeded.'),
+        error => console.error(error)
+      );
+    }
+
+    this.selectedItem = null;
+
+    if (isNewItem && pressedEnter) {
+      setTimeout(() => this.addItem(), 250);
+    }
+  }
+
+  deleteItem(item: TodoItemDto, countDown?: boolean) {
+    if (countDown) {
+      if (this.deleting) {
+        this.stopDeleteCountDown();
+        return;
+      }
+      this.deleteCountDown = 3;
+      this.deleting = true;
+      this.deleteCountDownInterval = setInterval(() => {
+        if (this.deleting && --this.deleteCountDown <= 0) {
+          this.deleteItem(item, false);
+        }
+      }, 1000);
+      return;
+    }
+    this.deleting = false;
+    if (this.itemDetailsModalRef) {
+      this.itemDetailsModalRef.hide();
+    }
+
+    if (item.id === 0) {
+      const itemIndex = this.selectedList.items.indexOf(this.selectedItem);
+      this.selectedList.items.splice(itemIndex, 1);
+    } else {
+      this.itemsClient.delete(item.id).subscribe(
+        () =>
+        (this.selectedList.items = this.selectedList.items.filter(
+          t => t.id !== item.id
+        )),
+        error => console.error(error)
+      );
+    }
+  }
+
+  stopDeleteCountDown() {
+    clearInterval(this.deleteCountDownInterval);
+    this.deleteCountDown = 0;
+    this.deleting = false;
+  }
+
+  
+}
